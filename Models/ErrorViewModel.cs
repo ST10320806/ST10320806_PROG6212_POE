d319522f3af457392df57eaaa8841e4ac0631b7b
Jesse-Lee Weeder <ST10320806@vcconnect.edu.za>
2024-09-09 00:04:12 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10320806_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
